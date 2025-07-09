884a3585e1dc0c1e342ea335a8053fc264c904ad
25janet <149666488+25janet@users.noreply.github.com>
2025-07-09 10:15:28 +0300

    Intorduction to database

---
+++ MySQLServer.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password= "JanetWanjiry@2005"
+        )
+        if connection.is_connected():
+            mycursor = connection.cursor()
+            mycursor.execute("CREATE DATABSE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+    finally:
+        if connection.is_connected():
+            mycursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    create_database()
